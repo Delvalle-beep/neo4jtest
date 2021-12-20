9c44c2862d82eabb2e0f1c7f5a55efc0484b2173
Oliveira <olivev13@nala.roche.com>
2021-12-20 12:28:07 -0300

    New comments

--- app.js
+++ app.js
@@ -23,49 +23,47 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Aonde está neo4j.auth.basic('login','senha') alterar os campos
 
 app.get('/', async function(req, res){
-   
-
-    var config = {
+         var config = {
         container_id: "viz",
         server_url:"bolt://824d2fd3.databases.neo4j.io",
         server_user: "neo4j",
         server_password: "cWiIFqGtm45j2kVFVIeqliZKEZSrGSb-UB1kgNrEG10",
         labels: {
             "Team":{
-                "caption":"name",
-                "size": 1.0
+                "caption": "name",
+                "size": "pagerank",
+                "community": "community",
+                "title_properties": [
+                    "name"
+                ],
             },
             "Member": {
-                "caption": "name",
-                "size": 1.0,
                 "community": "community"
             },
             "Initative":{
-                "caption":"name",
-                "size": 1.0
             }
         },
         relationships: {
             "Merber_Of": {
             },
-            "Working_ON": {
-                
+            "Working_ON": {   
             }
         },
-        
+        arrows: true,
         initial_cypher: `LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/Meisam-A/ER/main/dummy%20to%20be%20visualized.xlsx%20-%20member_of.csv' AS row_t
                          LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/Meisam-A/ER/main/dummy%20to%20be%20visualized.xlsx%20-%20working_on.csv' AS row_i
-                         MATCH (t:Team {Name: row_t.Team}),
-                         (n:Member {Name: row_t.Member_T}),
+                         MATCH (t:Team {name: row_t.Team}),
+                         (n:Member {name: row_t.Member_T}),
                          p = shortestPath((n)-[:Member_Of]->(t))
-                         MATCH (i:Initative {Name: row_i.Initative}),
-                         (m:Member {Name: row_i.Member_I}),
+                         MATCH (i:Initative {name: row_i.Initative}),
+                         (m:Member {name: row_i.Member_I}),
                          q = shortestPath((m)-[:Working_ON]->(i))
                          RETURN p,q`,
+
         encrypted: "ENCRYPTION_OFF",
         trust: "TRUST_SYSTEM_CA_SIGNED_CERTIFICATES"
     };
-
+//SET linhas após o titulo, renomear label atual 
     res.render('index', {configs: config});
 })
 
@@ -74,5 +72,7 @@ app.listen(3000);
 module.exports = app;
 
 //depois de tudo instalar via terminal o nodemon versão global
-// comando "npm install nodemon -g"
-//"nodemon app.js"
+// run "npm install" at the first time
+// comando "npm install nodemon -g" 
+
+//"nodemon app.js" or "node app.js"
